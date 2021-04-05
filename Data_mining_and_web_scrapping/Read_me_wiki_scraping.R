2a0eb701756945e0aa243417d2e8307310f516e9
Rūta Kondrot <71491238+RutaKondrot@users.noreply.github.com>
2021-04-05 18:41:48 +0300

    Update Read_me_wiki_scraping.R

--- Data_mining_and_web_scrapping/Read_me_wiki_scraping.R
+++ Data_mining_and_web_scrapping/Read_me_wiki_scraping.R
@@ -1,1 +1,6 @@
+## wiki_scraping.R
+### Data frames ("coaches", "coaches_clean" & "coaches_plot_clean")
+[data_frames.zip](https://github.com/RutaKondrot/Data-mining-and-web-scrapping/files/6258812/data_frames.zip)
 
+### The stacked bar chart "Coaches of FC Bayern Munich since 1963" 
+![Rplot](https://user-images.githubusercontent.com/71491238/113588004-47b03f80-9638-11eb-84d5-884a449538e1.png)
